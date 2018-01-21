a957c0eb5582fb28edcb320511663c39bd437f78
ZhengCoder <zheng.coder@gmail.com>
2018-01-22 00:04:05 +0800

    adjust widget height

--- src/components/dashboard/MyTestTab4.vue
+++ src/components/dashboard/MyTestTab4.vue
@@ -1,12 +1,30 @@
 <template>
   <div class="charts-page">
-    <div class="row">
+    <div class="row my-test-tab-4">
       <div class="col-md-6">
         <vuestic-widget class="chart-widget" :headerText="'charts.verticalBarChart' | translate">
-          <vuestic-chart v-bind:data="verticalBarChartData" type="vertical-bar"></vuestic-chart>
+          123
         </vuestic-widget>
       </div>
       <div class="col-md-6">
+        <vuestic-widget class="chart-widget" :headerText="'charts.horizontalBarChart' | translate">
+          456
+        </vuestic-widget>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-md-4">
+        <vuestic-widget class="chart-widget" :headerText="'charts.horizontalBarChart' | translate">
+          <vuestic-chart v-bind:data="horizontalBarChartData" type="horizontal-bar"></vuestic-chart>
+        </vuestic-widget>
+      </div>
+      <div class="col-md-4">
+        <vuestic-widget class="chart-widget" :headerText="'charts.horizontalBarChart' | translate">
+          <vuestic-chart v-bind:data="horizontalBarChartData" type="horizontal-bar"></vuestic-chart>
+        </vuestic-widget>
+      </div>
+      <div class="col-md-4">
         <vuestic-widget class="chart-widget" :headerText="'charts.horizontalBarChart' | translate">
           <vuestic-chart v-bind:data="horizontalBarChartData" type="horizontal-bar"></vuestic-chart>
         </vuestic-widget>
@@ -33,9 +51,11 @@
 </script>
 
 <style lang="scss">
-  .widget.chart-widget {
-    .widget-body {
-      height: 550px;
+  .my-test-tab-4 {
+    .widget.chart-widget {
+      .widget-body {
+        height: 300px;
+      }
     }
   }
 </style>
